9ac57a93b007e1f083bb2f93fba0bbf025ba8a19
Vikms95 <vms1995@hotmail.com>
2022-01-16 05:48:02 +0100

    Add button inside divs to display and change bookReadState property

--- notes.md
+++ notes.md
@@ -1,4 +1,6 @@
 (X)- Function to loop to iterate myLibrary and display an element by creating a new div inside .cards-container
 (X)- Button new book = add eventlistener and prompts and alert to add new book, then another one to add book properties
-- Add button to each book display
-- Add eventListener to button remove object from list and update display with displayExistingLibrary
+(X) - Add remove button to each book display
+(X) - Add eventListener to button remove object from list and update display with displayExistingLibrary = use div.parentNode.remove()
+(X) - Add button to display and change the bookReadState property 
+ 
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const cardsContainerReference = document.querySelector(".cards-container");
 const newBookButtonReference = document.querySelector(".new-book");
 
-const theHobbit = new Book("Hobbit","Tolkien","800","unread");
-const harryPotter = new Book("Harry Potter","Rowling","310","read");
-const donQuijote = new Book("Don Quijote","Cervantes","785","unread");
+// const theHobbit = new Book("Hobbit","Tolkien","800","unread");
+// const harryPotter = new Book("Harry Potter","Rowling","310","read");
+// const donQuijote = new Book("Don Quijote","Cervantes","785","unread");
 
-let myLibrary = [theHobbit,harryPotter,donQuijote];
+let myLibrary = [];
 
 updateExistingLibrary(myLibrary);
 addEventListenerNewButton();
 
 function Book(title, author, pages, readState) {
+    
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -22,21 +23,6 @@ function Book(title, author, pages, readState) {
                 + " and right now it's state is " + this.readState
 
     } 
-}
-
-function addBookToLibrary(){
-    const bookTitle = window.prompt("Enter the book's name: ");
-    const bookAuthor = window.prompt("Enter book's author: ");
-    const bookPages = window.prompt("Enter book's number of pages: ");
-    const bookReadState = window.prompt("Have you finished this book?: ") ;
-
-    const book = new Book(bookTitle,bookAuthor,bookPages,bookReadState)
-    myLibrary.push(book);
-    return book;
-}
-
-function removeBookFromLibrary(){
-
 }
 
 function updateExistingLibrary(myLibrary){
@@ -51,17 +37,48 @@ function updateExistingLibrary(myLibrary){
     });
 }
 
+function addBookToLibrary(){
+    const bookTitle = window.prompt("Enter the book's name: ");
+    const bookAuthor = window.prompt("Enter book's author: ");
+    const bookPages = window.prompt("Enter book's number of pages: ");
+    const bookReadState = window.prompt("Have you finished this book?: ");
+
+    const book = new Book(bookTitle,bookAuthor,bookPages,bookReadState)
+    myLibrary.push(book);
+    return book;
+}
+
+function removeBookFromLibrary(){
+}
+
 function addEventListenerNewButton(){
     newBookButtonReference.addEventListener("click", () => {
         const bookToAdd = addBookToLibrary()
-       
-        const div = cardsContainerReference.appendChild(document.createElement("div"));
-        div.classList.add("book");
-        div.textContent = bookToAdd.displayBookInfo();
-
-        const button = div.appendChild(document.createElement("button"));
-        button.classList.add("remove-button");
-        button.textContent = "Remove book"
-        
+        displayNewBook(bookToAdd);
     });
+}
+
+function displayNewBook(bookToAdd){
+    const div = cardsContainerReference.appendChild(document.createElement("div"));
+    div.classList.add("book");
+    div.textContent = bookToAdd.displayBookInfo();
+
+    const readButton = div.appendChild(document.createElement("button"));
+    readButton.classList.add("read-button");
+    readButton.textContent = bookToAdd.readState;
+    readButton.addEventListener("click", () => {
+        if(readButton.textContent === "Unread"){
+            readButton.textContent = "Read";
+        }else{
+            readButton.textContent = "Unread"
+        }
+        
+    })
+
+    const removeButton = div.appendChild(document.createElement("button"));
+    removeButton.classList.add("remove-button");
+    removeButton.textContent = "Remove book";
+    removeButton.addEventListener("click", () => {
+        removeButton.parentElement.remove();
+    })  
 }
