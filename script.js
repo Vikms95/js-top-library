42a1bd66dc95ac870459f8c24b591dc2ba91c461
Vikms95 <vms1995@hotmail.com>
2022-01-16 04:30:36 +0100

    Create function to display books and add books to the list

---
+++ script.js
@@ -0,0 +1,49 @@
+const cardsContainerReference = document.querySelector(".cards-container");
+const newBookButtonReference = document.querySelector(".new-book");
+
+const theHobbit = new Book("Hobbit","Tolkien","800","unread");
+const harryPotter = new Book("Harry Potter","Rowling","310","read");
+const donQuijote = new Book("Don Quijote","Cervantes","785","unread");
+
+
+let myLibrary = [theHobbit,harryPotter,donQuijote];
+
+displayLibrary(myLibrary);
+addEventListenerNewButton();
+
+function Book(title, author, pages, readState) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readState = readState;
+    this.displayBookInfo = function(){
+        return this.title 
+                + " by " + this.author 
+                + ", has " + this.pages + " pages, " 
+                + " and right now it's state is " + this.readState
+
+    } 
+}
+
+function addBookToLibrary(){
+    const bookTitle = window.prompt("Enter the book's name: ");
+    const bookAuthor = window.prompt("Enter book's author: ");
+    const bookPages = window.prompt("Enter book's number of pages: ");
+    const bookReadState = window.prompt("Have you finished this book?: ") ;
+
+    const book = new Book(bookTitle,bookAuthor,bookPages,bookReadState)
+
+    myLibrary.push(book);
+}
+
+function displayLibrary(myLibrary){
+    myLibrary.forEach(book => {
+        const div = cardsContainerReference.appendChild(document.createElement("div"));
+        div.classList.add("book")
+        div.textContent = book.displayBookInfo();
+    });
+}
+
+function addEventListenerNewButton(){
+    newBookButtonReference.addEventListener("click", addBookToLibrary);
+}
