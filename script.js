ba3a2a11174a8216c183dfc84b65c6ef4a78d4d1
Vikms95 <vms1995@hotmail.com>
2022-01-29 03:09:39 +0100

    Rework to implement OOP paradigm

--- script.js
+++ script.js
@@ -1,3 +1,130 @@
+class Book{   
+    constructor(title, author, pages, readState){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readState = readState;
+        this.displayBookInfo = () => {
+            return this.title 
+                    + " by " + this.author 
+                    + ", has " + this.pages + " pages, " 
+                    + " and right now it's state is: "
+
+        };
+    }; 
+
+    get getReadState(){
+        return this.readState;
+    }
+
+    set setReadState(state){
+        this.readState = state;
+    }
+
+    updateExistingLibrary(myLibrary){
+        let div;
+
+        if(Array.isArray(myLibrary)){
+            myLibrary.forEach(book => {
+                div = book.addBookToDiv();
+                book.addReadButton(div);
+                book.addRemoveButton(div);
+            })
+        }
+        else
+        {
+            div = myLibrary.addBookToDiv();
+            myLibrary.addReadButton(div);
+            myLibrary.addRemoveButton(div);
+        }
+    };
+
+    addEventListenerNewButton(){
+        newBookButtonReference.addEventListener("click", () => {
+            const bookToAdd = this.addBookToLibraryList();
+            if(bookToAdd === null) return;
+            this.updateExistingLibrary(bookToAdd);
+        });
+    };
+
+    isInputValid (bookPages,bookReadState){
+        if (isNaN(parseInt(bookPages))){
+            alert("Number of pages should be a number!");
+            return false;
+        }
+        else if(bookReadState.toLowerCase() !== "unread" && bookReadState.toLowerCase() !== "read"){
+            alert("Valid values are 'read' or 'unread'.");
+            return false;
+        }
+        else{
+            return true;
+        }
+    };
+
+    addBookToLibraryList(){
+        const bookTitle = window.prompt("Enter the book's name: ");
+        if(bookTitle === null) return null;
+        const bookAuthor = window.prompt("Enter the book's author: ");
+        if(bookAuthor === null) return null;
+        const bookPages = window.prompt("Enter the book's number of pages: ");
+        if(bookPages === null) return null;
+        const bookReadState = window.prompt("Have you finished this book? (Valid values => read/unread) ");
+        if(bookReadState === null) return null;
+
+        if(this.isInputValid(bookPages,bookReadState)){
+            const book = new Book(bookTitle,bookAuthor,bookPages,bookReadState);
+            myLibrary.push(book);
+            return book;
+        }
+        else{
+            addBookToLibraryList();
+        }
+    };
+
+    addBookToDiv(){
+        const div = cardsContainerReference.appendChild(document.createElement("div"));
+        div.classList.add("book");
+        div.textContent = this.displayBookInfo();
+        return div;
+    };
+
+    addReadButton(div){
+        const readButton = div.appendChild(document.createElement("button"));
+        readButton.classList.add("read-button");
+        readButton.textContent = this.getReadState;
+        if(this.getReadState == "unread"){
+            readButton.textContent = "Unread";
+            readButton.style.color = "red";
+        }else{
+            readButton.textContent = "Read";
+            readButton.style.color = "green";
+        }
+        readButton.addEventListener("click", () => {
+        if(this.getReadState === "unread"){
+            this.setReadState = 'read';
+            readButton.textContent = "Read"
+            readButton.style.color = "green";
+            return;
+        }else if(this.getReadState === "read"){
+            this.setReadState = 'unread';
+            readButton.textContent = "Unread"
+            readButton.style.color = "red";
+            return;
+        }else{
+            return;
+        }
+        })
+    };
+
+    addRemoveButton(div){
+        const removeButton = div.appendChild(document.createElement("button"));
+        removeButton.classList.add("remove-button");
+        removeButton.textContent = "Remove book";
+        removeButton.addEventListener("click", () => {
+            removeButton.parentElement.remove();
+        })  
+    };
+};
 const cardsContainerReference = document.querySelector(".cards-container");
 const newBookButtonReference = document.querySelector(".new-book");
 
@@ -37,123 +164,5 @@ let myLibrary = [
 	hamlet
 ];
 
-updateExistingLibrary(myLibrary);
-addEventListenerNewButton();
-
-function Book(title, author, pages, readState){   
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readState = readState;
-    this.displayBookInfo = function(){
-        return this.title 
-                + " by " + this.author 
-                + ", has " + this.pages + " pages, " 
-                + " and right now it's state is: "
-
-    }; 
-};
-
-function updateExistingLibrary(myLibrary){
-    let div;
-
-    if(Array.isArray(myLibrary)){
-        myLibrary.forEach(book => {
-            div = addBookToDiv(book);
-            addReadButton(div,book);
-            addRemoveButton(div);
-        })
-    }
-    else
-    {
-        div = addBookToDiv(myLibrary);
-        addReadButton(div,myLibrary);
-        addRemoveButton(div);
-    }
-};
-
-function addEventListenerNewButton(){
-    newBookButtonReference.addEventListener("click", () => {
-        const bookToAdd = addBookToLibraryList();
-        if(bookToAdd === null) return;
-        updateExistingLibrary(bookToAdd);
-    });
-};
-
-function isInputValid (bookPages,bookReadState){
-	if (isNaN(parseInt(bookPages))){
-		alert("Number of pages should be a number!");
-		return false;
-	}
-	else if(bookReadState.toLowerCase() !== "unread" && bookReadState.toLowerCase() !== "read"){
-		alert("Valid values are 'read' or 'unread'.");
-		return false;
-	}
-	else{
-		return true;
-	}
-};
-
-function addBookToLibraryList(){
-    const bookTitle = window.prompt("Enter the book's name: ");
-    if(bookTitle === null) return null;
-    const bookAuthor = window.prompt("Enter the book's author: ");
-    if(bookAuthor === null) return null;
-    const bookPages = window.prompt("Enter the book's number of pages: ");
-    if(bookPages === null) return null;
-    const bookReadState = window.prompt("Have you finished this book? (Valid values => read/unread) ");
-    if(bookReadState === null) return null;
-
-    if(isInputValid(bookPages,bookReadState)){
-		const book = new Book(bookTitle,bookAuthor,bookPages,bookReadState);
-		myLibrary.push(book);
-		return book;
-	}
-	else{
-		addBookToLibraryList();
-	}
-};
-
-function addBookToDiv(book){
-    const div = cardsContainerReference.appendChild(document.createElement("div"));
-    div.classList.add("book");
-    div.textContent = book.displayBookInfo();
-    return div;
-};
-
-function addReadButton(div,book){
-    const readButton = div.appendChild(document.createElement("button"));
-    readButton.classList.add("read-button");
-    readButton.textContent = book.readState;
-	if(book.readState == "unread"){
-		readButton.textContent = "Unread";
-		readButton.style.color = "red";
-	}else{
-		readButton.textContent = "Read";
-		readButton.style.color = "green";
-	}
-    readButton.addEventListener("click", () => {
-    if(book.readState === "unread"){
-		book.readState = "read";
-		readButton.textContent = "Read"
-        readButton.style.color = "green";
-		return;
-    }else if(book.readState === "read"){
-		book.readState = "unread";
-		readButton.textContent = "Unread"
-        readButton.style.color = "red";
-		return;
-    }else{
-		return;
-	}
-    })
-};
-
-function addRemoveButton(div){
-    const removeButton = div.appendChild(document.createElement("button"));
-    removeButton.classList.add("remove-button");
-    removeButton.textContent = "Remove book";
-    removeButton.addEventListener("click", () => {
-        removeButton.parentElement.remove();
-    })  
-};
+Book.prototype.updateExistingLibrary(myLibrary);
+Book.prototype.addEventListenerNewButton();
